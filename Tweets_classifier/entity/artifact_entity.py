84d3a4a8b202bef350d17cbb118f99edf3f9dc81
vivek mishra <starvivek1996@gmail.com>
2023-04-05 15:36:35 +0530

    data ingestion completed

---
+++ Tweets_classifier/entity/artifact_entity.py
@@ -0,0 +1,5 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
---
+++ Tweets_classifier/entity/config_entity.py
@@ -0,0 +1,26 @@
+import os,sys
+from Tweets_classifier.exception import SensorException
+from Tweets_classifier.logger import logging
+from datetime import datetime
+
+FILE_NAME = "Tweets.csv"
+
+
+class TrainingPipelineConfig:
+
+    def __init__(self):
+        try:
+            self.artifact_dir = os.path.join(os.getcwd(),"artifact",f"{datetime.now().strftime('%m%d%Y__%H%M%S')}")
+        except Exception  as e:
+            raise SensorException(e,sys)     
+        
+
+class DataIngestionConfig:
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        try:
+            self.DATA_FILE_PATH="https://raw.githubusercontent.com/VivekMishra96/datasets/main/Tweets.csv"
+            self.data_ingestion_dir = os.path.join(training_pipeline_config.artifact_dir , "data_ingestion")
+            self.feature_store_file_path = os.path.join(self.data_ingestion_dir,"feature_store",FILE_NAME)
+
+        except Exception as e:
+            return SensorException(e,sys)
