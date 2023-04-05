6d5b52f4a8f7e76decc231cb364316798d84b28e
vivek mishra <starvivek1996@gmail.com>
2023-04-05 17:04:09 +0530

    data transformation done

--- Tweets_classifier/entity/config_entity.py
+++ Tweets_classifier/entity/config_entity.py
@@ -4,6 +4,11 @@ from Tweets_classifier.logger import logging
 from datetime import datetime
 
 FILE_NAME = "Tweets.csv"
+INPUT_FEATURE_FILE_NAME = "feature.npz"
+TARGET_FILE_NAME = "target.npz"
+PARTY_FILE_NAME = "party.npz"
+TRANSFORMER_OBJECT_FILE_NAME = "cv_transformer.pkl"
+MODEL_FILE_NAME = "model.pkl"
 
 
 class TrainingPipelineConfig:
@@ -24,3 +29,33 @@ class DataIngestionConfig:
 
         except Exception as e:
             return SensorException(e,sys)
+        
+
+
+
+class DataValidationConfig:
+    pass
+
+
+        
+
+class DataTransformationConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        try:
+            self.data_transformation_dir = os.path.join(training_pipeline_config.artifact_dir , "data_transformation")
+            self.transform_object_path = os.path.join(self.data_transformation_dir,"transformer",TRANSFORMER_OBJECT_FILE_NAME)
+            self.transformed_feature_path =  os.path.join(self.data_transformation_dir,"transformed",INPUT_FEATURE_FILE_NAME)
+            self.transformed_target_path =os.path.join(self.data_transformation_dir,"transformed",TARGET_FILE_NAME)
+
+
+        except Exception as e:
+            return SensorException(e,sys)
+
+
+class ModelTrainerConfig:
+
+    def __init__(self,training_pipeline_config:TrainingPipelineConfig):
+        self.model_trainer_dir = os.path.join(training_pipeline_config.artifact_dir , "model_trainer")
+        self.model_path = os.path.join(self.model_trainer_dir,"model",MODEL_FILE_NAME)
+        self.expected_score = 0.8
